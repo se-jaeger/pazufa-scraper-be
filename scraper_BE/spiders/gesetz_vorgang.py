b532933476b22a56557abe8fbae8614377b155ec
Sebastian Jäger <se-jaeger@noreply.codeberg.org>
2026-03-19 22:56:10 +0100

    feat/gestz-vorgang-scraper (#9)
    Co-authored-by: Sebastian Jäger <git@sebastian-jaeger.me>
    Reviewed-on: https://codeberg.org/PaZuFa/scraper-BE/pulls/9

---
+++ scraper_BE/spiders/gesetz_vorgang.py
@@ -0,0 +1,43 @@
+from collections.abc import AsyncGenerator, Iterable
+from pathlib import Path
+
+import scrapy
+import scrapy.core.scheduler
+from pydantic import ValidationError
+from scrapy import Request
+from scrapy.http import XmlResponse
+
+from scraper_BE.pardok import GesetzVorgang
+from scraper_BE.spiders.utils import convert_element_to_dict
+
+
+class GesetzVorgangSpider(scrapy.Spider):
+    """A Scrapy spider for scraping process ('Vorgang') documents from the Berlin Parliament."""
+
+    name = "gesetz-vorgang"
+    allowed_domains = ["parlament-berlin.de"]
+    start_url_template = "https://www.parlament-berlin.de/opendata/pardok-wp{}.xml"
+
+    async def start(self) -> AsyncGenerator:
+        yield Request(url=self.start_url_template.format(self.crawler.settings.getint("WAHLPERIODE")))
+
+    def parse(self, response: XmlResponse) -> Request | scrapy.Item | Iterable[Request | scrapy.Item] | None:
+
+        cache_dir = Path(self.crawler.settings.get("CACHE_DIR")) / str(self.crawler.settings.getint("WAHLPERIODE")) / "feeds"
+        feed_file = cache_dir / f"{response.selector.xpath('/Export').attrib.get('aktualisiert')}.xml"
+
+        if not cache_dir.exists():
+            cache_dir.mkdir(parents=True)
+
+        if not feed_file.exists():
+            feed_file.write_text(response.text)
+
+        gesetz_vorgang_dicts = [convert_element_to_dict(x) for x in response.selector.xpath("/Export/Vorgang[./VTyp[text() = 'Gesetz']]")]
+
+        for gesetz_vorgang_dict in gesetz_vorgang_dicts:
+            try:
+                gesetz_vorgang = GesetzVorgang(**gesetz_vorgang_dict)
+                yield gesetz_vorgang
+
+            except ValidationError as error:
+                yield {"dict": gesetz_vorgang_dict, "error": error}
---
+++ scraper_BE/spiders/utils.py
@@ -0,0 +1,32 @@
+from lxml.etree import Element, QName
+from scrapy import Selector
+
+
+def convert_element_to_dict(node: Element | Selector, attributes=True) -> dict:
+    # NOTE: https://gist.github.com/jacobian/795571
+
+    node = node if isinstance(node, Element) else node.root
+
+    result = {}
+    if attributes:
+        for item in node.attrib.items():
+            key, result[key] = item
+
+    for element in node.iterchildren():
+        # Remove namespace prefix
+        key = QName(element).localname
+
+        # Process element as tree element if the inner XML contains non-whitespace content
+        value = element.text if element.text and element.text.strip() else convert_element_to_dict(element)
+
+        if key in result:
+            if type(result[key]) is list:
+                result[key].append(value)
+
+            else:
+                result[key] = [result[key], value]
+
+        else:
+            result[key] = value
+
+    return result
